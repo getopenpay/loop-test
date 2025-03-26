593f5bb301d8b98982ec882d63be8a8c15a3aba3
Nate Parke <nathaniel.parke@gmail.com>
2025-03-26 08:19:30 -0700

    Do it

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,47 @@ import { initLoopConnect, LoopConnectPayIn } from "@loop-crypto/connect";
 import './App.css'
 
 initLoopConnect({
-  apiKey: "964d4532-3ce4-45b2-a339-192dc46ebc76",
-  entityId: "dc1a2948-c72a-439e-9f0f-788e4c4cad80",
-  merchantId: "dc1a2948-c72a-439e-9f0f-788e4c4cad80",
+  // TODO: Add your keys/configs here.
+  apiKey: "",
+  entityId: "",
+  merchantId: "",
+  environment: "demo",
+  onInitialized: ({ entityId }) => { console.log(entityId) },
+  onInitFailed:	({ type, message, data }) => { console.log(`${type} ${message} ${data}`) },
+  onWalletChange: ({ address }) => console.log(`${address}`),
+  onNetworkChange: ({ id, name, chain }) => console.log(`${id} ${name} ${chain}`),
 });
 
 function App() {
   return (
     <>
       <div className="App">
-        <LoopConnectPayIn paymentUsdAmount={100} />
+        <LoopConnectPayIn
+          paymentUsdAmount={100}
+          suggestedAuthorizationUsdAmount={10000}
+          minimumAuthorizationUsdAmount={10000}
+          awaitConfirmation={true}
+          onWalletChange={(detail) => {console.log('Loop wallet changed: ', detail)}}
+          onNetworkChange={(detail) => {console.log('Loop network changed: ', detail)}}
+          onPayInCustomerCreated={(customer) => {
+            console.log('customer created')
+            console.log(customer)
+          }}
+          onPayInStateChange={({ state, message, data }) => {
+            if (state != 'complete') {
+              console.log(
+                'Received state change for payin but ignoring because it is not complete.',
+                message,
+                data
+              );
+              return;
+            }
+            console.log('Loop Payin complete: ', data);
+          }}
+          onPayInFailed={(detail) => {
+            console.log('Loop Payin failed: ', detail);
+          }}
+        />
       </div>
     </>
   )
